097966724517b3fdfd903e50c0d5002e2ae2b770
Rupesh Pakhrin <70186413+rupace57@users.noreply.github.com>
2022-05-17 15:32:22 +0545

    Update ContactList.js

--- crud-app/src/components/ContactList.js
+++ crud-app/src/components/ContactList.js
@@ -22,7 +22,7 @@ const ContactList = (props) => {
     <div className="main">
       <h2>
         DATA        <Link to="/add">
-          <button className="ui button red right">Add Contact</button>
+          <button className="ui button red right">Add Data</button>
         </Link>
       </h2>
       <div className="ui celled table">{renderContactList}</div>
